0a2e00d4c37d9ba8b8017bbb23183428d32ce631
siddharth <sid.prasher@gmail.com>
2018-09-13 22:27:16 +0200

    Async and TPL

---
+++ LearningCSharp/LearningCSharp/Product.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LearningCSharp
+{
+    public class Product
+    {
+        readonly string name;
+        readonly double price;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public double Price { get { return price; } }
+
+        public Product(string n, double p)
+        {
+            name = n;
+            price = p;
+        }
+
+        public static List<Product> GetSampleProducts()
+        {
+            var result = new List<Product>();
+            result.Add(new Product("Lego", 10.2d));
+            result.Add(new Product("Cars", 4.5));
+
+
+            return result;
+        }
+    }
+
+    public class ProductComparer : IComparer<Product>
+    {
+        public int Compare(Product x, Product y)
+        {
+            if (x == null && y == null)
+                return 0;
+            if (x == null)
+                return -1;
+            else if (y == null)
+                return 1;
+            else return x.Name.CompareTo(y.Name);
+        }
+    }
+}
---
+++ LearningCSharp/LearningCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Product.GetSampleProducts();
+
+            // Sort style 1 -- call the IComparer object for each list item
+            list.Sort(new ProductComparer());
+
+            // sort style 2 --> delegate to an inline anonymous type
+            // this uses delegates
+            list.Sort(delegate (Product x, Product y) { return x.Name.CompareTo(y.Name); });
+
+            // sort style 3 --> lambda expression
+            list.Sort((x, y) => { return x.Name.CompareTo(y.Name); });
+
+            // sort style 4 --> Linq
+
+            list.OrderBy(p => p.Name);
+
+            foreach (var i in list)
+                Console.WriteLine(string.Format("Product:{0}; Price:{1}", i.Name, i.Price));
+            Console.Read();
+        }
+    }
+
+    public class GradeBook
+    {
+
+    }
+
+    public class Student
+    {
+        GradeBook gb;
+    }
+}
